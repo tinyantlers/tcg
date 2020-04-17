898c8eb4f364ca4e6d1f5ff3617db81330831e35
LuisMaya <mayafluis@gmail.com>
2020-04-17 11:41:14 -0700

    Contact kinda working

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,12 @@
 <?php
-require_once('phpmailer/PHPMailer.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+echo $_POST['email'];
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer();
